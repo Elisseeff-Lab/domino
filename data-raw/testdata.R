2de68b57aac1373111ffaf529c08c54934e0ba72
Jacob Mitchell <jmitch81@jhmi.edu>
2023-11-27 11:31:53 -0500

    data-raw installs from original hosts
    Scripts in data-raw detailing how vignette data was downloaded and processed now downloads the PBMC3K expression data and CellPhoneDB v4.0.0 data tables from their original hosts rather than downloading rehosted data from Zenodo.

--- data-raw/testdata.R
+++ data-raw/testdata.R
@@ -1,29 +1,57 @@
 # generate objects for comparison in testing scripts
 
+library(Seurat)
 library(domino2, lib = "../domino_libraries/libraries/v0_2_1/")
 
-# code to prepare `pbmc3k` dataset
-pbmc.data <- Seurat::Read10X(data.dir = "../inst/extdata/pbmc3k_filtered_gene_bc_matrices/")
-pbmc <- Seurat::CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
-pbmc[["percent.mt"]] <- Seurat::PercentageFeatureSet(pbmc, pattern = "^MT-")
-pbmc <- Seurat::NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
-pbmc <- Seurat::FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
-pbmc <- Seurat::ScaleData(pbmc, features = rownames(pbmc))
-pbmc <- Seurat::RunPCA(pbmc, features = Seurat::VariableFeatures(object = pbmc), verbose = FALSE)
-pbmc <- Seurat::FindNeighbors(pbmc, dims = 1:10)
-pbmc <- Seurat::FindClusters(pbmc, resolution = 0.5)
-pbmc <- Seurat::RunUMAP(pbmc, dims = 1:10)
-# Annotate clusters with cell phenotypes
-cell_dict <- data.frame(cluster = c(0:8), cell_type = c("naive_CD4_T_cell", "CD14_monocyte", "memory_CD4_T_cell", "B_cell", "CD8_T_cell", "CD16_monocyte", "NK_cell", "dendritic_cell", "platelet"))
-pbmc$cell_type <- plyr::mapvalues(pbmc$seurat_clusters, from = cell_dict$cluster, to = cell_dict$cell_type)
+# load data for generation of test results from zenodo repository
+# Zenodo host of outputs from SCENIC analysis
+data_url <- "https://zenodo.org/records/10161143/files"
+temp_dir <- tempdir()
+
+pbmc_dir <- paste0(temp_dir, "/pbmc")
+if (!dir.exists(pbmc_dir)) {
+  dir.create(pbmc_dir)
+}
+# Seurat object of preprocessed PBMC3K data
+download.file(url = paste0(data_url, "/pbmc_seurat.rds"),
+              destfile = paste0(pbmc_dir, "/pbmc_seurat.rds"))
+pbmc <- readRDS(paste0(pbmc_dir, "/pbmc_seurat.rds"))
+
+# SCENIC input files
+scenic_dir <- paste0(temp_dir, "/scenic")
+if (!dir.exists(scenic_dir)) {
+  dir.create(scenic_dir)
+}
+download.file(url = paste0(data_url, "/scenic_auc_pbmc_3k.csv"),
+              destfile = paste0(scenic_dir, "/auc_pbmc_3k.csv"))
+download.file(url = paste0(data_url, "/scenic_regulons_pbmc_3k.csv"),
+              destfile = paste0(scenic_dir, "/regulons_pbmc_3k.csv"))
+auc <- read.table(paste0(scenic_dir, "/auc_pbmc_3k.csv"),
+                  header = TRUE, row.names = 1,
+                  stringsAsFactors = FALSE, sep = ",")
+regulons <- read.csv(paste0(scenic_dir, "/regulons_pbmc_3k.csv"))
+
+# CellPhoneDB Database
+cellphone_url <- "https://github.com/ventolab/cellphonedb-data/archive/refs/tags/v4.0.0.tar.gz"
+cellphone_tar <- paste0(temp_dir, "/cellphoneDB_v4.tar.gz")
+download.file(url = cellphone_url, destfile = cellphone_tar)
+cellphone_dir <- paste0(temp_dir, "/cellphoneDB_v4")
+untar(tarfile = cellphone_tar, exdir = cellphone_dir)
+cellphone_data <- paste0(cellphone_dir, "/cellphonedb-data-4.0.0/data")
+
+interactions <- read.csv(paste0(cellphone_data, "/interaction_input.csv"), stringsAsFactors = FALSE)
+complexes <- read.csv(paste0(cellphone_data, "/complex_input.csv"), stringsAsFactors = FALSE)
+genes <- read.csv(paste0(cellphone_data, "/gene_input.csv"), stringsAsFactors = FALSE)
+proteins <- read.csv(paste0(cellphone_data, "/protein_input.csv"), stringsAsFactors = FALSE)
 
 # subset the pbmc data to fewer cells to meet package requirements
 RNA_features <- c("FAS", "FASLG", "ITGAM", "ITGB2", "FCER2", "LCK", "CD8A", "CD8B", "C5", "C5AR1")
 TF_features <- c("CLOCK", "TAF1", "BCL6")
 name_features <- c("FAS", "FASLG", "integrin_aMb2_complex", "FCER2", "LCK", "CD8_receptor", "C5", "C5AR1")
 cell_types_dwn <- c("CD8_T_cell", "NK_cell", "B_cell")
-n <- 100
+n <- 120
 cell_list <- list()
+set.seed(123)
 for(i in seq_along(cell_types_dwn)){
   cell <- cell_types_dwn[i]
   cell_barcodes <- colnames(pbmc)[pbmc$cell_type == cell]
@@ -43,29 +71,20 @@ RNA_zscore_test <- pbmc@assays$RNA@scale.data[
   rownames(pbmc@assays$RNA@scale.data) %in% RNA_features, 
   colnames(pbmc@assays$RNA@scale.data) %in% barcodes_dwn]
 
-# cellphoneDB database
-cellphonedb_4_path <- "../inst/extdata/cellphoneDB_v4/"
-complexes <- read.csv(paste0(cellphonedb_4_path, "/complex_input.csv"), stringsAsFactors = FALSE)
+# subset CellPhoneDB inputs
 complexes_test <- complexes[complexes$complex_name %in% name_features,]
-genes <- read.csv(paste0(cellphonedb_4_path, "/gene_input.csv"), stringsAsFactors = FALSE)
 genes_test <- genes[genes$gene_name %in% RNA_features,]
-proteins <- read.csv(paste0(cellphonedb_4_path, "/protein_input.csv"), stringsAsFactors = FALSE)
 proteins_test <- proteins[proteins$uniprot %in% genes_test$uniprot,]
-interactions <- read.csv(paste0(cellphonedb_4_path, "/interaction_input.csv"), stringsAsFactors = FALSE)
 interactions_test <- interactions[
   (interactions$partner_a %in% proteins_test$uniprot | interactions$partner_a %in% complexes_test$complex_name) & 
     (interactions$partner_b%in% proteins_test$uniprot | interactions$partner_b %in% complexes_test$complex_name),]
 
-# read in and subset SCENIC inputs
-auc <- read.table(
-  "../inst/extdata/scenic/auc_pbmc_3k.csv",
-  header = TRUE, row.names = 1, stringsAsFactors = FALSE, sep = ","
-)
+
+# subset SCENIC inputs
 auc <- t(auc)
 rownames(auc) <- gsub("\\.\\.\\.$", "", rownames(auc))
 auc_test <- auc[TF_features, barcodes_dwn]
 
-regulons <- read.csv("../inst/extdata/scenic/regulons_pbmc_3k.csv")
 regulons <- regulons[-1:-2, ]
 colnames(regulons) <- c("TF", "MotifID", "AUC", "NES", "MotifSimilarityQvalue", "OrthologousIdentity", "Annotation", "Context", "TargetGenes", "RankAtMax")
 regulons_test <- regulons[regulons$TF %in% TF_features,]
