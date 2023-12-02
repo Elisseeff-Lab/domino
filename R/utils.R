d844444cbab6c0357e0c6af22b000f507fc200a7
dimalvovs <dlvovs1@jh.edu>
2023-12-02 11:23:13 -0500

    DRY python bool conversions

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
 #'
 #' @param arg The argument to check
 #' @param allow_class Vector of allowed classes
-#' @param allw_len Vector of allowed lengths
+#' @param allow_len Vector of allowed lengths
 check_arg <- function(arg, allow_class = c("character"), allow_len = NULL) {
   argname <- deparse(substitute(arg))
   classes <- paste(allow_class, collapse = ",")
@@ -19,4 +19,19 @@ check_arg <- function(arg, allow_class = c("character"), allow_len = NULL) {
       stop(sprintf("Length of %s must be one of: %s", argname, lengths))
     }
   }
+}
+
+
+#' Change cases of True/False syntax from Python to TRUE/FALSE R syntax
+#' 
+#' @param obj Object that will be converted
+#'
+conv_py_bools <- function(obj) {
+  for (x in colnames(obj)) {
+    bools <- sort(unique(obj[[x]]))
+    if (identical(bools, c("False", "True"))) {
+      obj[[x]] <- ifelse(obj[[x]] == "True", TRUE, FALSE)
+    }
+  }
+  return(obj)
 }
