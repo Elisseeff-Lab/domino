75ff6647f59eda927134f14902995529f5427315
dimalvovs <dlvovs1@jh.edu>
2024-01-04 16:49:44 -0500

    roxygenize, add cache fun

--- R/utils.R
+++ R/utils.R
@@ -251,7 +251,7 @@ dom_network_items <- function(dom, clusters = NULL, return = NULL) {
 #' Specify the cache
 #' 
 #' @param path Path to tstore cache on
-#' 
+#' @export
 cache <- function(path = tools::R_user_dir(package = "domino2")) {
   bfc <- BiocFileCache::BiocFileCache(path, ask = FALSE)
 
