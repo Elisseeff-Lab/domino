d592633345d0b4aa6b6597daf2fcc1231ee987e3
dimalvovs <dlvovs1@jh.edu>
2024-01-10 11:25:11 -0500

    simplify & use default Bioc location

--- tests/testthat/test-cache.r
+++ tests/testthat/test-cache.r
@@ -1,7 +1,7 @@
 test_that("can add to a new cache", {
   tf <- tempfile(tmpdir = tempdir(check = TRUE))
   file.create(tf)
-  bfc <- cache(path = tempdir())
+  bfc <- BiocFileCache::BiocFileCache(tempdir(), ask = FALSE)
   expect_equal(length(bfc), 0)
   BiocFileCache::cleanbfc(bfc, ask = FALSE)
   BiocFileCache::removebfc(bfc, ask = FALSE)
