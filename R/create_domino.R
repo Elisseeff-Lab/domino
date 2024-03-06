176538d106ad3eec1a76e8c14ed18ee955c6459c
Jacob Mitchell <jmitch81@jhmi.edu>
2024-03-06 15:25:39 -0500

    initial read_rl_map_complexes
    Finds ligand and receptor complexes to write a list of components in each complex. Also provides a warning for cases where a complex occurs multiple times in the rl_map, but with different component genes.

--- R/create_domino.R
+++ R/create_domino.R
@@ -1,1 +1,38 @@
 # internal scripts for the create_domino() function
+
+read_rl_map_complexes <- function(rl_parse, use_complexes) {
+  if(use_complexes) {
+    complex_lig <- rl_parse[grepl("\\,", rl_parse$L.gene), "L.name"]
+    complex_rec <- rl_parse[grepl("\\,", rl_parse$R.gene), "R.name"]
+    complex_names <- unique(c(complex_lig, complex_rec))
+    names(complex_names) <- complex_names
+    gene_complexes <- lapply(
+      complex_names, FUN = function(x) {
+        comp_cs <- c(
+          rl_parse[rl_parse$L.name == x, "L.gene"],
+          rl_parse[rl_parse$R.name == x, "R.gene"]
+        )
+        if(length(unique(comp_cs)) > 1) {
+          comp_first <- comp_cs[1]
+          warning(
+            paste0(
+              "protein complex ", x, 
+              " has multiple, differing descriptions of component genes. \n",
+              "Defaulting to first encountered component description: \n",
+              comp_first
+            )
+          )
+          comp <- unique(unlist(strsplit(comp_first, split = "\\,")))
+          return(comp)
+        } else {
+          comp <- unique(unlist(strsplit(comp_cs, split = "\\,")))
+          return(comp)
+        }
+      }
+    )
+    return(gene_complexes)
+  } else {
+    return(NULL)
+  }
+}
+
