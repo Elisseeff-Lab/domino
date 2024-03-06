e2cd820dc6e596beb4360c5d00a82d119bff2dd7
Jacob Mitchell <jmitch81@jhmi.edu>
2024-03-06 10:21:48 -0500

    rename variables

--- R/create_domino.R
+++ R/create_domino.R
@@ -5,23 +5,23 @@ parse_rl_map_genes <- function(rl_map) {
     rl_map, MARGIN = 1,
     FUN = function(inter) {
       rl <- c()
-      p <- ifelse(inter[["type_A"]] == "R", "A", "B")
+      p <- ifelse(x[["type_A"]] == "R", "A", "B")
       q <- ifelse(p == "A", "B", "A")
-      R.gene <- inter[[paste0("gene_", p)]]
-      L.gene <- inter[[paste0("gene_", q)]]
+      R.gene <- x[[paste0("gene_", p)]]
+      L.gene <- x[[paste0("gene_", q)]]
       rl[["R.gene"]] <- R.gene
       rl[["L.gene"]] <- L.gene
-      if (paste0("uniprot_", p) %in% names(inter)) {
-        rl[["R.uniprot"]] <- inter[[paste0("uniprot_", p)]]
+      if (paste0("uniprot_", p) %in% names(x)) {
+        rl[["R.uniprot"]] <- x[[paste0("uniprot_", p)]]
       }
-      if (paste0("uniprot_", q) %in% names(inter)) {
-        rl[["L.uniprot"]] <- inter[[paste0("uniprot_", q)]]
+      if (paste0("uniprot_", q) %in% names(x)) {
+        rl[["L.uniprot"]] <- x[[paste0("uniprot_", q)]]
       }
-      if (paste0("name_", p) %in% names(inter)) {
-        rl[["R.name"]] <- inter[[paste0("name_", p)]]
+      if (paste0("name_", p) %in% names(x)) {
+        rl[["R.name"]] <- x[[paste0("name_", p)]]
       }
-      if (paste0("name_", q) %in% names(inter)) {
-        rl[["L.name"]] <- inter[[paste0("name_", q)]]
+      if (paste0("name_", q) %in% names(x)) {
+        rl[["L.name"]] <- x[[paste0("name_", q)]]
       }
       return(rl)
     }
