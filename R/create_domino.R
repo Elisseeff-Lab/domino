1a8dd295b699ddcaba5766bd90f7298d19caba16
Jacob Mitchell <jmitch81@jhmi.edu>
2024-03-08 16:15:38 -0500

    initial select_cluster_tf
    Initial function for selection of TFs associated with each cluster cluster assignment in the expression data. This was constructed with the intention of allowing code to be added for other options of how to identify the TFs associated with a cluster, starting with one-sided wilcoxon tests.

--- R/create_domino.R
+++ R/create_domino.R
@@ -1,1 +1,36 @@
 # internal scripts for the create_domino() function
+
+select_cluster_tf <- function(features, clusters, verbose = TRUE, method = "one.sided.wilcox") {
+  cluster_lvls <- levels(clusters)
+  n_lvls <- length(cluster_lvls)
+  feat_names <- rownames(features)
+  names(feat_names) <- feat_names
+  if(verbose) {message("Calculating feature enrichment by cluster")}
+  cl_pval_list <- lapply(
+    cluster_lvls,
+    function(cl) {
+      a <- which(cluster_lvls == cl)
+      if(verbose) {message(paste0(a, " of ", n_lvls))}
+      cl_cells <- which(clusters == cl)
+      out_cells <- which(clusters != cl)
+      p_val_ls <- lapply(
+        feat_names,
+        function(f) {
+          cl_value <- features[f, cl_cells]
+          out_value <- features[f, out_cells]
+          if(method == "one.sided.wilcox") {
+            test_res <- stats::wilcox.test(
+              x = cl_value, y = out_value, alternative = "greater"
+            )
+            return(test_res[["p.value"]])
+          }
+        }
+      )
+      p_vals <- unlist(p_val_ls)
+      return(p_vals)
+    }
+  )
+  pval_mat <- do.call(cbind, cl_pval_list)
+  colnames(pval_mat) <- cluster_lvls
+  return(pval_mat)
+}
