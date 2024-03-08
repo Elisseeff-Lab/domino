ebf60595e14a3e39c880c3342fe41ad031b6d379
Jacob Mitchell <jmitch81@jhmi.edu>
2024-03-08 14:20:06 -0500

    write_rec_lig_linkages initial
    reads through parsed rl_map using lapply basis instead of a for loop to construct the list of ligands capable of interacting with each receptor.

--- R/create_domino.R
+++ R/create_domino.R
@@ -1,1 +1,16 @@
 # internal scripts for the create_domino() function
+
+write_rec_lig_linkages <- function(rl_parse) {
+  rec_names <- unique(rl_parse[["R.name"]])
+  names(rec_names) <- rec_names
+  rec_lig_ls <- lapply(
+    rec_names,
+    function(r) {
+      rl_int_row <- rl_parse[rl_parse[["R.name"]] == r,]
+      ligs <- rl_int_row[["L.name"]]
+      return(ligs)
+    }
+  )
+  return(rec_lig_ls)
+}
+
