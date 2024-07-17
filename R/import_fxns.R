7d237ef29248e5767400082070679498c005e172
Jacob Mitchell <jmitch81@jhmi.edu>
2024-07-17 15:45:09 -0400

    disable verbose create_domino example

--- R/import_fxns.R
+++ R/import_fxns.R
@@ -306,7 +306,9 @@ create_regulon_list_scenic <- function(regulons) {
 #'  rl_map = rl_map_tiny, features = SCENIC$auc_tiny,
 #'  counts = PBMC$RNA_count_tiny, z_scores = PBMC$RNA_zscore_tiny,
 #'  clusters = PBMC$clusters_tiny, tf_targets = regulon_list_tiny,
-#'  use_clusters = TRUE, use_complexes = TRUE, remove_rec_dropout = FALSE)
+#'  use_clusters = TRUE, use_complexes = TRUE, remove_rec_dropout = FALSE,
+#'  verbose = FALSE
+#'  )
 #'
 #' pbmc_dom_tiny_no_clusters <- create_domino(
 #'  rl_map = rl_map_tiny, features = SCENIC$auc_tiny,
@@ -314,7 +316,9 @@ create_regulon_list_scenic <- function(regulons) {
 #'  clusters = PBMC$clusters_tiny, tf_targets = regulon_list_tiny,
 #'  use_clusters = FALSE, use_complexes = FALSE,
 #'  rec_min_thresh = 0.1, remove_rec_dropout = TRUE,
-#'  tf_selection_method = "all")
+#'  tf_selection_method = "all",
+#'  verbose = FALSE
+#'  )
 #'
 create_domino <- function(
     rl_map, features, counts = NULL, z_scores = NULL,
